ec3ce81ce741a00a3e243adb52624ddfcc42f91b
Jacob Jershin <jane@williams.org>
2022-03-24 03:17:06 -0500

    last one 3am gn

--- OSZtoIGIS/Source/ScenesShell/Background.swift
+++ OSZtoIGIS/Source/ScenesShell/Background.swift
@@ -5,36 +5,54 @@ import Foundation
   /*
      This class is responsible for rendering the background.
    */
-let CS = 4*9
+
+let CS = 4*35
 let X = 115
 let Y = 212
+
+func sqrt(num:Int)-> Double {
+    var x1:Double = (Double(num) * 1.0) / 2;
+    var x2:Double = (x1 + (Double(num) / x1)) / 2;
+    while(abs(x1 - x2) >= 0.0000001){
+        x1 = x2;
+        x2 = (x1 + (Double(num) / x1)) / 2;
+    }
+    return Double(x2);
+}
 
 class Background : RenderableEntity {
 
     let hitCircle : Image
-    
-    init() {
-        guard let hitCirclePNG = URL(string:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/US-WhiteHouse-Logo.svg/2000px-US-WhiteHouse-Logo.svg.png") else {
-            fatalError("Failed to create URL for whitehouse")
+    let Number : Image
 
-            // Using a meaningful name can be helpful for debugging
-          super.init(name:"Background")
+    init() {
+        guard let HitObjURL = URL(string:"https://codermerlin.com/users/jacob-jershin/assets/hitcircle.png") else {
+            fatalError("Failed to create URL for HitObj")
+        }
+        guard let NumberURL = URL(string:"https://codermerlin.com/users/jacob-jershin/assets/default-1%402x.png") else {
+            fatalError("Failed to create URL for HitObj1")
+        }
+
+        hitCircle = Image(sourceURL:HitObjURL)
+        Number = Image(sourceURL:NumberURL)
+        
+        // Using a meaningful name can be helpful for debugging
+        super.init(name:"Background")
     }
     override func setup(canvasSize:Size, canvas:Canvas) {
- 
+        canvas.setup(hitCircle)
+        canvas.setup(Number)
+    }
+    override func render(canvas:Canvas) {
+        if hitCircle.isReady {
+            hitCircle.renderMode = .destinationRect(Rect(topLeft:Point(x:X, y:Y), size:Size(width:CS, height:CS)))
+            canvas.render(hitCircle)
+        }
 
+        if Number.isReady {
+            Number.renderMode = .destinationRect(Rect(topLeft:Point(x:X+Int(sqrt(num:CS*2)), y:Y+Int(sqrt(num:CS*2))), size:Size(width:CS/2, height:CS/2)))
+            canvas.render(Number)
+        }
         
-//circle code
-
-        /*
-        let lineWidth = LineWidth(width:5)
-        canvas.render(lineWidth)  
-
-        let red = StrokeStyle(color:Color(.red))
-        canvas.render(red)
-
-        let ellipse = Ellipse(center:Point(x:X, y:Y), radiusX:CS, radiusY:CS)
-        canvas.render(ellipse)
-         */
     }
 }
